da971b1e26752a4b209cb68c30f87dc1c24cf990
dannyhannyford <dannyhannyford@gmail.com>
2023-06-16 13:16:48 -0700

    arrayList, bubbleSort

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,17 @@
 export default class ArrayList<T> {
     public length: number;
 
-    
+    constructor() {}
 
-    constructor() {
-    }
-
-    prepend(item: T): void {
-
+    prepend(item: T): void {}
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
 }
-    insertAt(item: T, idx: number): void {
 
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
-}
+// array downsides
+// can't delete, only 0 out
+// cant inseart, only write
+// can't grow
--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,19 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            console.log(`arr[${i}]:${arr[i]} arr[${j}]:${arr[j]}`);
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
 }
+// every iteration
+// largest item will go to the end
+
+// sum of numbers between a range
+// ((N+1)N)/2 -> n^2 + n -> n^2
+
+// immutability
+// Pros: easy to use
+//  cons: space, replicating space
