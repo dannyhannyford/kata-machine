751fd20ad16018029a59e3c9796acf6256bacc1d
dannyhannyford <dannyhannyford@gmail.com>
2023-06-16 13:15:12 -0700

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,25 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    // get lo
+    let l = 0;
+    // get high
+    let h = haystack.length - 1;
+    while (l <= h) {
+        let mid = Math.floor(l + (h - l) / 2);
+        if (haystack[mid] === needle) {
+            return true;
+        }
+        if (haystack[mid] < needle) {
+            l++;
+        }
+        if (haystack[mid] > needle) {
+            h--;
+        }
+    }
+    return false;
+    // function to check if target is our answer, lower, or higher
 }
+
+/**
+ * sorting list takes O(nlog n)
+ * Binary Search  takes O(log n)
+ */
